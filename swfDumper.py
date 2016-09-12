3c42a3e199825dec3a916afa0f72664b4bb41653
mini-ninja-64 <mini-ninja-64@users.noreply.github.com>
2016-09-12 20:11:10 +0100

    Slight change

--- swfDumper.py
+++ swfDumper.py
@@ -17,13 +17,23 @@ for file in os.listdir(os.getcwd()):
 for file in os.listdir(os.getcwd()):
 	if file.endswith(".swf"):
 		command = "swfrender " + os.path.abspath(file) + " -X " + str(width) + " -o " + dir + "/" + os.path.splitext(file)[0] + ".png"
-		os.system(command)
+		#os.system(command)
+		time.sleep(0.25)
+
+		#recheck size
+		(columns,lines) = shutil.get_terminal_size((80, 20))
 		percent = numOfFilesProcessed/numOfFiles
 
 		loadingBar = ''
 		for i in range(0,int((columns+1)*percent)):
 			loadingBar += '#'
 
-		sys.stdout.write("\r" + loadingBar)
-		sys.stdout.flush()
+		#Clear and write
+		sys.stdout.write("\033[K") #clear line
+		print(loadingBar)
+		sys.stdout.write("\033[F") #back to previous line
+
 		numOfFilesProcessed += 1
+
+print()
+print("Dump Completed!")
